8a7b0a29f0bc8d18931548ade83d2ba6d3185377
Utsav Agrawal <utsavagrawal98@gmail.com>
2018-04-07 18:59:08 +0530

    Update carModel.js

--- carModel.js
+++ carModel.js
@@ -22,3 +22,28 @@ function drawCar(car){
       rect(car.x+car.h*3/8, car.y-12.5, car.w/4, car.h);
     }
 }
+
+function drawAmbulance(car) {
+  rectMode(CENTER);
+  if (car.head.top) {
+    fill("WHITE");
+    rect(car.x - 12.5, car.y, car.w, car.h);
+    fill("BLUE");
+    rect(car.x - 12.5, car.y - car.h * 3 / 8, car.w, car.h / 4);
+  } else if (car.head.bottom) {
+    fill("WHITE");
+    rect(car.x + 12.5, car.y, car.w, car.h);
+    fill("BLUE");
+    rect(car.x + 12.5, car.y + car.h * 3 / 8, car.w, car.h / 4);
+  } else if (car.head.left) {
+    fill("WHITE");
+    rect(car.x, car.y + 12.5, car.w, car.h);
+    fill("BLUE");
+    rect(car.x - car.h * 3 / 8, car.y + 12.5, car.w / 4, car.h);
+  } else if (car.head.right) {
+    fill("WHITE");
+    rect(car.x, car.y - 12.5, car.w, car.h);
+    fill("BLUE");
+    rect(car.x + car.h * 3 / 8, car.y - 12.5, car.w / 4, car.h);
+  }
+}

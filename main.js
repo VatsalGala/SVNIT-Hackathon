4775cb56c3df549e301b2efaf9b964b8083e4870
Sagar Jyani <jyanisagar36@gmail.com>
2018-04-07 16:59:34 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -12,4 +12,8 @@ function draw()
 {
 	background(100);
 	drawMap();
+	// car = {x:475,y:475,head:{top:false, bottom:false,
+	// 						left:true, right:false},
+	// 		w:15,h:15}
+	// drawCar(car);
 }
